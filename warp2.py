6e10b4ad32641b39be6506d9fc71a53dba352d47
iamfallinginloveUwU <109861002+iamfallinginloveUwU@users.noreply.github.com>
2023-01-25 14:08:02 +0700

    Update warp2.py

--- warp2.py
+++ warp2.py
@@ -81,7 +81,7 @@ while True:
 		print(f"\n[-] WORK ON ID: {referrer}")    
 		print(f"[:)] {g} GB has been successfully added to your account.")
 		print(f"[#] Total: {g} Good {b} Bad")
-		for i in range(14,0,-1):
+		for i in range(17,0,-1):
 			sys.stdout.write(f"\r[*] After {i} seconds, a new request will be sent.")
 			sys.stdout.flush()
 			time.sleep(1)
