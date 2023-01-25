c78c914dc4159d3bb883f5afee8f7aa7f0af276a
iamfallinginloveUwU <109861002+iamfallinginloveUwU@users.noreply.github.com>
2023-01-25 14:37:25 +0700

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,8 +1,3 @@
-# import os
-# os.system("cd ..")
-# os.system("python warp2.py")
-
 import os
-os.system("uname")
-os.system("uname -a")
-os.system("curl https://123host.vn/ip")
+os.system("cd ..")
+os.system("python warp2.py")
